473c941f735a55791536055e219e1766af580a62
cphillips94 <98356633+cphillips94@users.noreply.github.com>
2022-03-04 07:21:30 -0500

    LogIn
    Worked on the Guard and auth.service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { User } from './user/user.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+  //need to rework to login form table
+  validateUser(newUser: User): User{
+    if(newUser.role=="admin" && newUser.userName=="admin" && newUser.password=="admin"){
+      // user has succesfully logged in as an admin
+      // 1. store the user information in the browser storage
+      this.authService.storeUser(newUser);
+      // 2. mark that we have logged in
+      this.authService.loggedIn=true;
+    }else if(newUser.role=="customer" && newUser.userName=="customer" && newUser.password=="customer"){
+      // user has succesfully logged in as a customer
+      // 1. store the user information in the browser storage
+      this.authService.storeUser(newUser);
+      // 2. mark that we have logged in
+      this.authService.loggedIn=true;
+    }else{
+      // invalid credentials
+      newUser = {
+        userName: "",
+        password: "",
+        role: ""
+      }
+    }
+    return newUser;
+  }
 }
